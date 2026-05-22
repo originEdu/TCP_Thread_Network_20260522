e9e6ca939f3c9e6d3047bfa5e6395eaab27b56fe
originEdu <originpark82@gmail.com>
2026-05-22 15:21:44 +0900

    Initial commit

---
+++ Client/client.cpp
@@ -0,0 +1,372 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define main SDL_main
+
+#include "ChatPacket.h"
+#include "NetUtil.h"
+
+#include <winsock2.h>
+#include <Windows.h>
+#include <iostream>
+#include <process.h>
+#include <conio.h>
+#include <mutex>
+#include "SDL.h"
+
+#pragma comment(lib, "ws2_32")
+#pragma comment(lib, "NetCommon")
+
+
+using namespace std;
+
+char SendBuffer[1024] = { 0, };
+char RecvBuffer[1024] = { 0, };
+
+bool IsRecvThreadRunning = true;
+bool IsSendThreadRunning = true;
+bool IsRanderThreadRunning = true;
+
+//다른 클라 정보관리
+SessionManager MySessionManager;
+
+//내 소켓 아이디
+SOCKET MyClientID;
+
+void ProcessPacket(SOCKET ProcessSocket, const char* InBuffer, const Header& InHeader); // Recv받은 패킷에 따라 역할수행
+unsigned WINAPI RecvThread(void* Argument); 
+unsigned WINAPI SendThread(void* Argument); 
+unsigned WINAPI RanderThread(void* Argument);
+
+void Render(SDL_Renderer* MyRender); //랜더 함수
+void HideCursor(); //커서 숨기기 함수
+
+SDL_Window* InitWindow(); //윈도우Init
+SDL_Renderer* CreateRender(SDL_Window* window); //랜더러 생성
+
+//렌더러 및 이벤트 -> 귀찮으니 전역으로
+SDL_Window* MyWindow = InitWindow();
+SDL_Renderer* MyRender = CreateRender(MyWindow);
+SDL_Event event;
+
+//세션Lock
+std::mutex sessionLock;
+int main(int argc, char* argv[])
+{
+	srand((unsigned short)time(nullptr));
+	HideCursor();
+
+	WSAData wsaData;
+
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET ServerSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN ServerSockAddr;
+	memset(&ServerSockAddr, 0, sizeof(ServerSockAddr));
+	ServerSockAddr.sin_family = AF_INET;
+	ServerSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	//ServerSockAddr.sin_addr.s_addr = inet_addr("192.168.0.95");
+	ServerSockAddr.sin_port = htons(35000);
+
+	connect(ServerSocket, (SOCKADDR*)&ServerSockAddr, sizeof(ServerSockAddr));
+
+	cout << "client connect" << endl;
+
+	C2S_Login LoginData;
+	LoginData.UserID = "Test";
+	LoginData.HashKey = "1as3f356dsd6gyhg";
+
+	Header LoginHeader;
+	LoginHeader.MakeHeader(static_cast<unsigned short>(LoginData.ToString().length()), EPacketType::C2S_Login);
+
+	//Login 요청
+	SendAll(ServerSocket, (char*)&LoginHeader, HeaderSize);
+	SendAll(ServerSocket, LoginData.ToString().c_str(), (int)LoginData.ToString().length());
+	cout << "Login 요청함" << endl;
+
+	HANDLE ThreadHandles[3] = { 0, };
+
+	//nonblocking, asynchrous
+	ThreadHandles[0] = (HANDLE)_beginthreadex(0, 0, RecvThread, &ServerSocket, /*CREATE_SUSPENDED*/0, 0);
+	ThreadHandles[1] = (HANDLE)_beginthreadex(0, 0, SendThread, &ServerSocket, /*CREATE_SUSPENDED*/0, 0);
+	ThreadHandles[2] = (HANDLE)_beginthreadex(0, 0, RanderThread, MyRender, /*CREATE_SUSPENDED*/0, 0);
+
+	//SDL 이벤트
+	while (IsRanderThreadRunning) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				IsRanderThreadRunning = false; 
+			}
+			else if (event.type == SDL_KEYDOWN)
+			{
+				SDL_Keycode pressedKey = event.key.keysym.sym;
+
+				const char* KeyCode = SDL_GetKeyName(pressedKey);
+				
+				if (pressedKey == 'w' ||
+					pressedKey == 'a' ||
+					pressedKey == 's' ||
+					pressedKey == 'd')
+				{
+					C2S_Move Data;
+					Data.ClientSocket = MyClientID;
+					Data.Direction = *KeyCode;
+					Header DataHeader;
+					DataHeader.MakeHeader((int)(Data.ToString().length()), EPacketType::C2S_Move);
+					//header
+					int SentBytes = SendAll(ServerSocket, (char*)&DataHeader, HeaderSize);
+					if (SentBytes <= 0)
+					{
+						cout << "C2S_Move header send fail." << endl;
+					}
+					//Data
+					SentBytes = SendAll(ServerSocket, Data.ToString().c_str(), (int)(Data.ToString().length()));
+					if (SentBytes <= 0)
+					{
+						cout << "C2S_Move Data send fail." << endl;
+					}
+				}
+			}
+		}
+		
+		SDL_Delay(1);
+	}
+
+	//blocking
+	WaitForMultipleObjects(3, ThreadHandles, FALSE, INFINITE);
+
+	closesocket(ServerSocket);
+
+	IsSendThreadRunning = false;
+	IsRecvThreadRunning = false;
+	IsRanderThreadRunning = false;
+
+	CloseHandle(ThreadHandles[0]);
+	CloseHandle(ThreadHandles[1]);
+
+	WSACleanup();
+
+
+	SDL_DestroyRenderer(MyRender);
+	SDL_DestroyWindow(MyWindow);
+	SDL_Quit();
+
+	return 0;
+}
+
+void ProcessPacket(SOCKET ProcessSocket, const char* InBuffer, const Header& InHeader)
+{
+	switch ((EPacketType)InHeader.PacketType)
+	{
+	case EPacketType::S2C_Login:
+	{
+		S2C_Login LoginPacket;
+		LoginPacket.Parse(InBuffer);
+		MyClientID = LoginPacket.ClientSocketID;
+		std::cout << LoginPacket.ClientSocketID << LoginPacket.Message << std::endl;
+	}
+	break;
+	case EPacketType::S2C_Spawn:
+	{
+		S2C_Spawn SpawnData;
+		SpawnData.Parse(InBuffer);
+		std::cout << SpawnData.ToString().c_str() << std::endl;
+
+		//다른 클라이언트 정보 가지고 있는 세션매니저에 자기정보 저장
+		//원래는 세션이 아니라 엑터 매니저
+		Session Insession;
+		Insession.ClientSocket = SpawnData.ClientSocket;
+		Insession.Shape = SpawnData.Shape;
+		Insession.X = SpawnData.X;
+		Insession.Y = SpawnData.Y;
+		Insession.R = SpawnData.R;
+		Insession.G = SpawnData.G;
+		Insession.B = SpawnData.B;
+		MySessionManager.Add(Insession);
+		Render(MyRender);
+	}
+	break;
+	case EPacketType::S2C_Move:
+	{
+		S2C_Move MoveData;
+		MoveData.Parse(InBuffer);
+		Session* FindSession = MySessionManager.GetSession(MoveData.ClientSocket);;
+		FindSession->X = MoveData.X;
+		FindSession->Y = MoveData.Y;
+
+		//std::cout << MoveData.ToString() << std::endl;
+		Render(MyRender);
+	}
+	break;
+	case EPacketType::S2C_Destroy:
+	{
+		S2C_Destroy DestroyPacket;
+		DestroyPacket.Parse(InBuffer);
+		std::cout << DestroyPacket.ClientSocket << std::endl;
+		sessionLock.lock(); //Rander쓰레드에 사용하고 있으므로 Lock
+		Session* FindSession = MySessionManager.GetSession(DestroyPacket.ClientSocket);
+		MySessionManager.Delete(*FindSession);
+		sessionLock.unlock();
+		Render(MyRender);
+	}
+	break;
+	}
+}
+
+unsigned WINAPI RecvThread(void* Argument)
+{
+	SOCKET ServerSocket = *(SOCKET*)Argument;
+
+	while (IsRecvThreadRunning)
+	{
+		Header HeaderData;
+		//header
+		int RecvBytes = RecvAll(ServerSocket, (char*)&HeaderData, sizeof(HeaderData));
+		if (RecvBytes <= 0)
+		{
+			cout << "recv fail " << endl;
+			break;
+		}
+		HeaderData.NetworkToHost();
+
+		memset(RecvBuffer, 0, sizeof(RecvBuffer));
+		//data JSON
+		RecvBytes = RecvAll(ServerSocket, RecvBuffer, HeaderData.PacketSize);
+		if (RecvBytes <= 0)
+		{
+			cout << "recv fail " << endl;
+			break;
+		}
+
+		ProcessPacket(ServerSocket, RecvBuffer, HeaderData);
+	
+	}
+	return 0;
+}
+
+unsigned WINAPI SendThread(void* Argument)
+{
+	//책임은 사용하는 놈이 진다.
+	SOCKET ServerSocket = *(SOCKET*)Argument;
+
+	while (IsSendThreadRunning)
+	{
+		int KeyCode = _getch();
+		KeyCode = toupper(KeyCode);
+		if (KeyCode == 'W' ||
+			KeyCode == 'A' ||
+			KeyCode == 'S' ||
+			KeyCode == 'D')
+		{
+			C2S_Move Data;
+			Data.ClientSocket = MyClientID;
+			Data.Direction = (char)KeyCode;
+			Header DataHeader;
+			DataHeader.MakeHeader((int)(Data.ToString().length()), EPacketType::C2S_Move);
+			//header
+			int SentBytes = SendAll(ServerSocket, (char*)&DataHeader, HeaderSize);
+			if (SentBytes <= 0)
+			{
+				cout << "C2S_Move header send fail." << endl;
+			}
+			//Data
+			SentBytes = SendAll(ServerSocket, Data.ToString().c_str(), (int)(Data.ToString().length()));
+			if (SentBytes <= 0)
+			{
+				cout << "C2S_Move Data send fail." << endl;
+			}
+		}
+		else if (KeyCode == 13)
+		{
+			cin.getline(SendBuffer, sizeof(SendBuffer));
+			ChatPacket Data;
+			Data.UserID = "Test";
+			Data.Message = SendBuffer;
+			Data.Gold = 0;
+			std::string JSONString = Data.ToString();
+			Header DataHeader;
+			DataHeader.MakeHeader((int)(Data.ToString().length()), EPacketType::ChatPacket);
+			//header
+			int SentBytes = SendAll(ServerSocket, (char*)&DataHeader, HeaderSize);
+			if (SentBytes <= 0)
+			{
+				cout << "C2S_Move header send fail." << endl;
+			}
+			//Data
+			SentBytes = SendAll(ServerSocket, Data.ToString().c_str(), (int)(Data.ToString().length()));
+			if (SentBytes <= 0)
+			{
+				cout << "C2S_Move Data send fail." << endl;
+			}
+		}
+
+	}
+
+	return 0;
+}
+
+unsigned WINAPI RanderThread(void* Argument)
+{
+	SDL_Renderer* Rander = (SDL_Renderer*)Argument;
+
+	while (IsRanderThreadRunning) {
+		SDL_SetRenderDrawColor(MyRender, 0, 0, 0, 255); // 배경을 검은색으로
+		SDL_RenderClear(MyRender);
+		sessionLock.lock();
+		for (auto Player : MySessionManager.SessionList)
+		{
+			SDL_Rect MyRect{ Player.X, Player.Y, 50, 50 };
+			SDL_SetRenderDrawColor(MyRender, (Uint8)Player.R, (Uint8)Player.G, (Uint8)Player.B, 255);
+			SDL_RenderFillRect(MyRender, &MyRect);
+		}
+		sessionLock.unlock();
+		SDL_RenderPresent(MyRender);
+		SDL_Delay(16);
+	}
+	return 0;
+}
+
+
+void Render(SDL_Renderer* MyRender)
+{
+	system("cls");
+	for (auto Player : MySessionManager.SessionList)
+	{
+		COORD Where;
+		Where.X = Player.X; 
+		Where.Y = Player.Y;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Where);
+		std::cout << (char)Player.Shape;
+	}
+}
+
+void HideCursor()
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cursorInfo;
+
+	GetConsoleCursorInfo(hConsole, &cursorInfo);
+	cursorInfo.bVisible = FALSE; //커서를 보이지 않게 설정
+	SetConsoleCursorInfo(hConsole, &cursorInfo);
+}
+
+SDL_Window* InitWindow()
+{
+	SDL_Window* window = SDL_CreateWindow(
+		"MyWindow",                       // 창 제목
+		SDL_WINDOWPOS_CENTERED,           // 창 시작 X 위치 (화면 중앙)
+		SDL_WINDOWPOS_CENTERED,           // 창 시작 Y 위치 (화면 중앙)
+		1000, 800,                       // 창 가로, 세로 크기
+		SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE              // 창을 바로 표시
+	);
+	return window;
+}
+
+SDL_Renderer* CreateRender(SDL_Window* window)
+{
+	SDL_Renderer* renderer = SDL_CreateRenderer(
+		window,
+		-1,
+		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC // 하드웨어 가속 및 수직동기화 사용
+	);
+	return renderer;
+}
---
+++ Server/server.cpp
@@ -0,0 +1,295 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include "NetUtil.h"
+
+#include <winsock2.h>
+#include <iostream>
+
+
+#pragma comment(lib, "ws2_32")
+#pragma comment(lib, "NetCommon")
+
+using namespace std;
+
+char Buffer[1024] = { 0, };
+
+SessionManager MySessionManager;
+
+void ProcessPacket(SOCKET ProcessSocket, const char* InBuffer, const Header& InHeader);
+
+void DisconnectSocket(SOCKET DisconnectedSocket, fd_set* Sockets);
+
+//blocking, synchrous, multiplexing(polling)
+int main()
+{
+	cout << "server start" << endl;
+
+	WSAData wsaData;
+
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET ListenSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN ListenSockAddr;
+	memset(&ListenSockAddr, 0, sizeof(ListenSockAddr));
+	ListenSockAddr.sin_family = AF_INET;
+	ListenSockAddr.sin_addr.s_addr = INADDR_ANY;
+	ListenSockAddr.sin_port = htons(35000);
+
+	//already use port 이미 포트 사용중
+	::bind(ListenSocket, (SOCKADDR*)&ListenSockAddr, sizeof(ListenSockAddr));
+
+	listen(ListenSocket, SOMAXCONN);
+
+
+
+	//blocking, synchronous(TimeOut)
+	TIMEVAL TimeOut;
+	TimeOut.tv_sec = 0;
+	TimeOut.tv_usec = 500000;
+
+	fd_set ReadSockets;
+	fd_set CopyReadSockets;
+
+	FD_ZERO(&ReadSockets);
+	FD_SET(ListenSocket, &ReadSockets);
+
+	while (true)
+	{
+		CopyReadSockets = ReadSockets;
+
+		//0.5초씩 blocking
+		int ChangeCount = select(0, &CopyReadSockets, 0, 0, &TimeOut);
+
+		if (ChangeCount <= 0)
+		{
+			//Server Work
+			//0.5초한번 서버 작업을 하는거
+			continue;
+		}
+
+		//몬가 자료 있다.
+		for (int i = 0; i < (int)ReadSockets.fd_count; ++i)
+		{
+			if (FD_ISSET(ReadSockets.fd_array[i], &CopyReadSockets))
+			{
+				if (ReadSockets.fd_array[i] == ListenSocket)
+				{
+					//connect process
+					SOCKADDR_IN ClientSockAddr;
+					memset(&ClientSockAddr, 0, sizeof(ClientSockAddr));
+					int ClientSockSockLength = sizeof(ClientSockAddr);
+
+					//blocking, synchronous
+					SOCKET ClientSocket = accept(ListenSocket, (SOCKADDR*)&ClientSockAddr, &ClientSockSockLength);
+
+					cout << "connect client " << inet_ntoa(ClientSockAddr.sin_addr) << endl;
+
+					FD_SET(ClientSocket, &ReadSockets);
+				}
+				else
+				{
+					//Data Receive
+
+					//header
+					Header DataHeader;
+					int RecvBytes = RecvAll(ReadSockets.fd_array[i], (char*)&DataHeader, HeaderSize);
+					if (RecvBytes <= 0)
+					{
+						cout << "header recv fail or disconnect " << endl;
+						DisconnectSocket(ReadSockets.fd_array[i], &ReadSockets);
+						continue;
+					}
+
+					DataHeader.NetworkToHost();
+
+					memset(Buffer, 0, sizeof(Buffer));
+					//data JSON
+					RecvBytes = RecvAll(ReadSockets.fd_array[i], Buffer, DataHeader.PacketSize);
+					if (RecvBytes <= 0)
+					{
+						cout << "data recv fail " << endl;
+						DisconnectSocket(ReadSockets.fd_array[i], &ReadSockets);
+						continue;
+					}
+					else
+					{
+						ProcessPacket(ReadSockets.fd_array[i], Buffer, DataHeader);
+					}
+				}
+			}
+		}
+	}
+
+	closesocket(ListenSocket);
+	WSACleanup();
+
+	return 0;
+}
+
+void ProcessPacket(SOCKET ProcessSocket, const char* InBuffer, const Header& InHeader)
+{
+	switch ((EPacketType)InHeader.PacketType)
+	{
+	case EPacketType::C2S_Login:
+		{
+			C2S_Login LoginPacket;
+			LoginPacket.Parse(InBuffer);
+			cout << LoginPacket.ToString() << endl;
+			//접속 한 유저 정보 업데이트(Session)
+			Session InSession;
+			InSession.ClientSocket = ProcessSocket;
+			InSession.UserID = LoginPacket.UserID;
+			InSession.X = rand() % 250 + 1;
+			InSession.Y = rand() % 250 + 1;
+			InSession.Shape = 65 + (rand() % 26);
+			InSession.R = 255;
+			InSession.G = rand() % 255;
+			InSession.B = rand() % 255;
+			MySessionManager.Add(InSession);
+
+			//접속 한 아이한테 확인 패킷(S2C_Login)
+			//헤더 및 데이터 던져주기
+			S2C_Login Data;
+			Data.ClientSocketID = ProcessSocket;
+			Data.Message = "로그인 성공";
+			Header DataHeader;
+			DataHeader.MakeHeader((int)(Data.ToString().length()), EPacketType::S2C_Login);
+			//header
+			int SentBytes = SendAll(ProcessSocket, (char*)&DataHeader, HeaderSize);
+			if (SentBytes <= 0)
+			{
+				cout << "S2C_Login header send fail." << endl;
+			}
+			//Data
+			SentBytes = SendAll(ProcessSocket, Data.ToString().c_str(), (int)(Data.ToString().length()));
+			if (SentBytes <= 0)
+			{
+				cout << "S2C_Login Data send fail." << endl;
+			}
+
+			//접속한 모든 유저한테 현재 모든 유저의 정보를 보내줌
+			for (auto Item : MySessionManager.SessionList)
+			{
+				S2C_Spawn SpawnData;
+				SpawnData.ClientSocket = Item.ClientSocket;
+				SpawnData.Shape = Item.Shape;
+				SpawnData.X = Item.X;
+				SpawnData.Y = Item.Y;
+				SpawnData.R = Item.R;
+				SpawnData.G = Item.G;
+				SpawnData.B = Item.B;
+
+				Header SpawnHeader;
+				SpawnHeader.MakeHeader((int)SpawnData.ToString().length(), EPacketType::S2C_Spawn);
+				for (auto Receiver : MySessionManager.SessionList)
+				{
+					//header
+					int SentBytes = SendAll(Receiver.ClientSocket, (char*)&SpawnHeader, HeaderSize);
+					if (SentBytes <= 0)
+					{
+						cout << "SpawnHeader send fail." << endl;
+					}
+
+					//Data
+					SentBytes = SendAll(Receiver.ClientSocket, SpawnData.ToString().c_str(), (int)(SpawnData.ToString().length()));
+					if (SentBytes <= 0)
+					{
+						cout << "SpawnData send fail." << endl;
+					}
+				}
+			}
+		}
+		break;
+	case EPacketType::C2S_Move:
+		{
+		C2S_Move MovePacket;
+		MovePacket.Parse(InBuffer);
+		Session* FindSession = MySessionManager.GetSession(MovePacket.ClientSocket);
+		switch (MovePacket.Direction)
+		{
+		case 'W':
+			FindSession->Y--;
+			break;
+		case 'S':
+			FindSession->Y++;
+			break;
+		case 'A':
+			FindSession->X--;
+			break;
+		case 'D':
+			FindSession->X++;
+			break;
+		}
+		//모든 유저한테 이동 패킷 보내기
+		S2C_Move MoveData;
+		MoveData.ClientSocket = FindSession->ClientSocket;
+		MoveData.X = FindSession->X;
+		MoveData.Y = FindSession->Y;
+		Header Header;
+		Header.MakeHeader((int)MoveData.ToString().length(), EPacketType::S2C_Move);
+
+		for (auto Receiver : MySessionManager.SessionList)
+		{
+			//header
+			int SentBytes = SendAll(Receiver.ClientSocket, (char*)&Header, HeaderSize);
+			if (SentBytes <= 0)
+			{
+				cout << "MoveHeader send fail." << endl;
+			}
+
+			//Data
+			SentBytes = SendAll(Receiver.ClientSocket, MoveData.ToString().c_str(), (int)(MoveData.ToString().length()));
+			if (SentBytes <= 0)
+			{
+				cout << "MoveData send fail." << endl;
+			}
+
+		}
+		}
+		break;
+	}
+}
+
+
+void DisconnectSocket(SOCKET DisconnectedSocket, fd_set* Sockets)
+{
+	SOCKADDR_IN ClosedSockAddr;
+	memset(&ClosedSockAddr, 0, sizeof(ClosedSockAddr));
+	int ClosedSockAddrLength = sizeof(ClosedSockAddr);
+
+	SOCKET ClosedSocket = DisconnectedSocket;
+	getpeername(ClosedSocket, (SOCKADDR*)&ClosedSockAddr, &ClosedSockAddrLength);
+	FD_CLR(DisconnectedSocket, Sockets);
+	closesocket(ClosedSocket);
+
+	//세션에서도 지워야함
+	S2C_Destroy DestroyPacket;
+	Session* FindSession = MySessionManager.GetSession(ClosedSocket);
+	DestroyPacket.ClientSocket = FindSession->ClientSocket;
+	MySessionManager.Delete(*FindSession);
+	FindSession = nullptr;
+	//모든 유저한테 보내기
+
+	Header Header;
+	Header.MakeHeader((int)DestroyPacket.ToString().length(), EPacketType::S2C_Destroy);
+
+	for (auto Receiver : MySessionManager.SessionList)
+	{
+		//header
+		int SentBytes = SendAll(Receiver.ClientSocket, (char*)&Header, HeaderSize);
+		if (SentBytes <= 0)
+		{
+			cout << "DestroyHeader send fail." << endl;
+		}
+
+		//Data
+		SentBytes = SendAll(Receiver.ClientSocket, DestroyPacket.ToString().c_str(), (int)(DestroyPacket.ToString().length()));
+		if (SentBytes <= 0)
+		{
+			cout << "DestroyData send fail." << endl;
+		}
+
+	}
+
+}
